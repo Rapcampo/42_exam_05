52bee4cf74037e03eb9e7d4728257330447c6cdf
Rapcampo <raphael.campospt@gmail.com>
2025-10-30 02:34:36 +0000

    added original map on test to be easier to see what is doing on

--- level2/bsq/full_tests.sh
+++ level2/bsq/full_tests.sh
@@ -7,7 +7,8 @@ echo "\n\n\n \e[1;32mINVALID MAPS!\e[0m"
 
 while IFS= read -r command; do
     echo "Executing map: $command"
+    echo -e "\n\e[1;33m\tMAP TILE\n\e[0m"
     cat $command
-    echo
+    echo -e "\n\e[1;31m\tAFTER BSQ\n\e[0m"
     valgrind -q --leak-check=full --show-leak-kinds=all ./bsq $command
 done < "$test_file"
